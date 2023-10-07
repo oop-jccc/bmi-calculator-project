697a6a713684f9d708d67d9925a3f228bec3464c
Damon German <damon.german@hyland.com>
2023-10-07 18:06:44 -0500

    Refactor BMI Calculator CLI to improve code readability
    Refactoring was done to the BMI Calculator CLI to enhance the code's readability and maintainability. The Main method has been simplified by moving redundant blocks of code into separate methods. Getting user weight and height inputs as well as unit type selection have been encapsulated into individual methods. This reduces clutter and makes the code more organized. The program's flow is now better structured, making it easier to understand and modify in the future.

--- bmi-calculator-cli/Program.cs
+++ bmi-calculator-cli/Program.cs
@@ -7,76 +7,24 @@ using bmi_calculator_core.Strategies;
 
 namespace bmi_calculator_cli;
 
-class Program
+internal static class Program
 {
-    static void Main(string[] args)
+    private static void Main()
     {
         while (true)
         {
-            Console.WriteLine("BMI Calculator");
-            Console.WriteLine("1. Enter weight and height");
-            Console.WriteLine("2. Exit");
-            Console.Write("Select an option: ");
+            DisplayMenu();
+
             var choice = Console.ReadLine();
 
             switch (choice)
             {
                 case "1":
-                    double weightInput;
-                    while (true)
-                    {
-                        Console.Write("Enter weight: ");
-                        if (double.TryParse(Console.ReadLine(), out weightInput) && weightInput > 0)
-                            break;
-                        Console.WriteLine("Invalid input. Please enter a positive number.");
-                    }
+                    var weight = GetWeight();
+                    var height = GetHeight();
+                    var unitType = GetUnitType();
 
-                    double heightInput;
-                    while (true)
-                    {
-                        Console.Write("Enter height: ");
-                        if (double.TryParse(Console.ReadLine(), out heightInput) && heightInput > 0)
-                            break;
-                        Console.WriteLine("Invalid input. Please enter a positive number.");
-                    }
-
-                    UnitType unitType;
-                    while (true)
-                    {
-                        Console.WriteLine("Select unit type:");
-                        Console.WriteLine("1. Metric");
-                        Console.WriteLine("2. Standard");
-                        var unitTypeInput = Console.ReadLine();
-                        if (unitTypeInput == "1")
-                        {
-                            unitType = UnitType.Metric;
-                            break;
-                        }
-
-                        if (unitTypeInput == "2")
-                        {
-                            unitType = UnitType.Standard;
-                            break;
-                        }
-
-                        Console.WriteLine("Invalid choice, please try again.");
-                    }
-
-                    var weight = new Weight(weightInput, unitType);
-                    var height = new Height(heightInput, unitType);
-
-                    IBMICalculatorStrategy bmiCalculatorStrategy = unitType == UnitType.Metric
-                        ? new MetricBMICalculatorStrategy()
-                        : new StandardBMICalculatorStrategy();
-
-                    var bmiCategoryInterpreter = new BMICategoryInterpreter();
-
-                    var bmiCalculator = new BMICalculator(bmiCalculatorStrategy, bmiCategoryInterpreter);
-
-                    var bmi = bmiCalculator.CalculateBMI(weight, height);
-                    var bmiCategory = bmiCalculator.GetBMICategory(bmi);
-
-                    Console.WriteLine($"Your BMI is {bmi:F1}. Category: {bmiCategory}");
+                    DisplayBMIResult(weight.Value, height.Value, unitType);
                     break;
 
                 case "2":
@@ -87,5 +35,75 @@ class Program
                     break;
             }
         }
+    }
+
+    private static void DisplayMenu()
+    {
+        Console.WriteLine("BMI Calculator");
+        Console.WriteLine("1. Enter weight and height");
+        Console.WriteLine("2. Exit");
+        Console.Write("Select an option: ");
+    }
+
+    private static double GetUserInput(string prompt)
+    {
+        double input;
+        while (true)
+        {
+            Console.Write(prompt);
+            if (double.TryParse(Console.ReadLine(), out input) && input > 0)
+                break;
+            Console.WriteLine("Invalid input. Please enter a positive number.");
+        }
+
+        return input;
+    }
+
+    private static Weight GetWeight()
+    {
+        var weightInput = GetUserInput("Enter weight: ");
+        return new Weight(weightInput, UnitType.Metric); // Defaulting to Metric, will be reset later
+    }
+
+    private static Height GetHeight()
+    {
+        var heightInput = GetUserInput("Enter height: ");
+        return new Height(heightInput, UnitType.Metric); // Defaulting to Metric, will be reset later
+    }
+
+    private static UnitType GetUnitType()
+    {
+        while (true)
+        {
+            Console.WriteLine("Select unit type:");
+            Console.WriteLine("1. Metric");
+            Console.WriteLine("2. Standard");
+            var unitTypeInput = Console.ReadLine();
+            if (unitTypeInput == "1")
+                return UnitType.Metric;
+            if (unitTypeInput == "2")
+                return UnitType.Standard;
+
+            Console.WriteLine("Invalid choice, please try again.");
+        }
+    }
+
+    private static void DisplayBMIResult(double weightValue, double heightValue, UnitType unitType)
+    {
+        var weight = new Weight(weightValue, unitType);
+        var height = new Height(heightValue, unitType);
+
+        IBMICalculatorStrategy bmiCalculatorStrategy = unitType == UnitType.Metric
+            ? new MetricBMICalculatorStrategy()
+            : new StandardBMICalculatorStrategy();
+
+        var bmiCategoryInterpreter = new BMICategoryInterpreter();
+
+        var bmiCalculator = new BMICalculator(bmiCalculatorStrategy, bmiCategoryInterpreter);
+
+        var bmi = bmiCalculator.CalculateBMI(weight, height);
+        var bmiCategory = bmiCalculator.GetBMICategory(bmi);
+
+        Console.WriteLine($"Your BMI is {bmi:F1}. Category: {bmiCategory}");
     }
 }
